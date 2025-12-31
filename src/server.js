a9d147b7e8de9711db8449dd26534f14f68124eb
fateenfatima <151643762+fateenfatima@users.noreply.github.com>
2025-12-31 09:02:32 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // Routes
 app.use("/api/events", eventRoutes);
 
-// ✅ SAFE PORT FIX
+// SAFE PORT FIX
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
